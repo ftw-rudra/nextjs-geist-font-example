83a9ac1345be18ac4dd651d025c4884d8ac46112
ftw-rudra <ftw.rudra@gmail.com>
2025-07-23 18:09:12 +0530

    Add .next/static/chunks/src_app_shop_page_tsx_137228e3._.js 1753274351985

---
+++ .next/static/chunks/src_app_shop_page_tsx_137228e3._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_461b5be0._.js"
+],
+    source: "dynamic"
+});
